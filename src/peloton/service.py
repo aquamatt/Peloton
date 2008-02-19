4cfd9e8cd687fe7f1f38748320fe967004a15246
mp <mp@4f9a194a-353e-0410-9ebf-c1faf78b6e41>
2008-02-19 15:28:42 +0000

    more stubs
    git-svn-id: https://svn.rethought-solutions.com/svn/peloton/trunk@23 4f9a194a-353e-0410-9ebf-c1faf78b6e41

--- src/peloton/service.py
+++ src/peloton/service.py
@@ -1,8 +1,31 @@
 # Copyright 2007 Matthew Pontefract
 # See LICENSE for details
 from svcdeco import testDecorator
+from peloton.utils.structs import ReadOnlyDict
 
 class PelotonService(object):
-    @testDecorator
-    def testMethod(self, x):
-        return x*x
+    """ Base class for all services. Public methods all have names prefixed
+'public_', much as twisted spread remote callable methods are prefixed 'remote_'.
+"""
+    def __init__(self, rootPath):
+        """ Root path passed in on construction because from this module
+cannot find where the sub-class lives. Configurations are found relative to this
+root path. """
+        self.rootPath = rootPath
+        self.loadConfig()
+        
+    def loadConfig(self):
+        """ Load the configuration and put into self.conf """
+        self.conf = ReadOnlyDict()
+        # locate config relative to root path of caller... 
+    
+    def setup(self):
+        """ Executed after configuration is loaded, prior to starting work.
+Can be used to setup database pools etc. Overide in actual services. """
+        pass
+    
+    def cleanup(self):
+        """ Executed prior to shuting down this service or the node.
+Can be used to cleanup database pools etc. Overide in actual services. """
+        pass
+    
