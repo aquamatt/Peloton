2456bd772c6b448fe1a9ba8a0a2d37111c361f9c
mp <mp@4f9a194a-353e-0410-9ebf-c1faf78b6e41>
2008-02-20 12:06:42 +0000

    sorted out license labels and copyright notice
    git-svn-id: https://svn.rethought-solutions.com/svn/peloton/trunk@24 4f9a194a-353e-0410-9ebf-c1faf78b6e41

--- src/peloton/__init__.py
+++ src/peloton/__init__.py
@@ -1,5 +1,13 @@
-# Copyright 2007 Matthew Pontefract
-# See LICENSE for details
+##############################################################################
+#
+# Copyright (c) 2007-2008 ReThought Limited and Peloton Contributors
+# All Rights Reserved.
+#
+# This software  is licensed under the terms of the BSD license, a copy of
+# which should accompany this distribution.
+#
+##############################################################################
+
 
 # The registry contains run-time configuration details.
 # By default the keys, once set, cannot be modified
--- src/peloton/persist/store.py
+++ src/peloton/persist/store.py
@@ -1,5 +1,13 @@
-# Copyright 2007 Matthew Pontefract
-# See LICENSE for details
+##############################################################################
+#
+# Copyright (c) 2007-2008 ReThought Limited and Peloton Contributors
+# All Rights Reserved.
+#
+# This software  is licensed under the terms of the BSD license, a copy of
+# which should accompany this distribution.
+#
+##############################################################################
+
 
 """ Interface for Peloton persistent, non-volatile storage. Likely
 adapters include ZODB/ZEO and possibly some SQL store """
