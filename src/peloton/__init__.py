b8ade445d38432cb502e203e7bee9f240806a927
mp <mp@4f9a194a-353e-0410-9ebf-c1faf78b6e41>
2008-02-26 10:17:39 +0000

    changed license tag at head of all sources
    git-svn-id: https://svn.rethought-solutions.com/svn/peloton/trunk@28 4f9a194a-353e-0410-9ebf-c1faf78b6e41

--- src/peloton/__init__.py
+++ src/peloton/__init__.py
@@ -1,13 +1,10 @@
-##############################################################################
+# $Id$
 #
 # Copyright (c) 2007-2008 ReThought Limited and Peloton Contributors
-# All Rights Reserved.
-#
-# This software  is licensed under the terms of the BSD license, a copy of
-# which should accompany this distribution.
-#
-##############################################################################
+# All Rights Reserved
+# See LICENSE for details
 
+RELEASE_VERSION = 0.0.1
 
 # The registry contains run-time configuration details.
 # By default the keys, once set, cannot be modified
