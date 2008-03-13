f155b57486e5b951ce4d4ceb52cd2f8ec0a27720
mp <mp@4f9a194a-353e-0410-9ebf-c1faf78b6e41>
2008-03-13 17:08:16 +0000

    commit prior to rename
    git-svn-id: https://svn.rethought-solutions.com/svn/peloton/trunk@70 4f9a194a-353e-0410-9ebf-c1faf78b6e41

--- src/peloton/router.py
+++ src/peloton/router.py
@@ -4,5 +4,18 @@
 # All Rights Reserved
 # See LICENSE for details
 
+"""Mapping consists of the following core tasks, each represented by
+a class in this module:
 
+    - Loading: determining what PSCs should be asked to start a service, and
+      getting them to do so (i.e. maps service to PSC).
+      
+    - Routing: Working out where a request should be sent to be satisfied, then
+      sending it (ie maps request to psc.service). 
+      
+The RoutingTable is required by all.
+"""
 
+class ServiceLoader(object):
+    def __init__(self, kernel):
+        
