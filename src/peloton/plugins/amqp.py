4efa661cee2230e5aa6f69ae2a0119cca5a9a7a3
mp <mp@4f9a194a-353e-0410-9ebf-c1faf78b6e41>
2008-03-18 18:30:43 +0000

    back from cafe
    git-svn-id: https://svn.rethought-solutions.com/svn/peloton/trunk@78 4f9a194a-353e-0410-9ebf-c1faf78b6e41

--- src/peloton/plugins/amqp.py
+++ src/peloton/plugins/amqp.py
@@ -7,13 +7,20 @@
 from peloton.plugins import PelotonPlugin
 from peloton.dispatcher import AbstractEventBusPlugin
 import amqplib.client_0_8 as amqp
+import threading
 
 class AMQPEventBus(PelotonPlugin,AbstractEventBusPlugin):
-    """
+    """Uses amqplib to hook into the AMQP event bus, most probably
+provided by RabbitMQ but potentially any provider. 
+
+The amqplib is not Twisted based and provides a blocking handler 
+for receiving messages off the bus. We thus run the listener
+in a thread and put call the message handler in the main thread.
 """
     def initialise(self):
         self.vhost = self.kernel.config['grid.messagingVHost']
         self.host = self.kernel.config['grid.messagingHost']
+        self.domain = self.kernel.initOptions.domain
     
     def start(self):
         self.amqpconn = amqp.Connection(self.host, 
@@ -21,16 +28,68 @@ class AMQPEventBus(PelotonPlugin,AbstractEventBusPlugin):
                                 password=self.config['password'], 
                                 ssl=False, 
                                 virtual_host=self.vhost)        
-        
+
         self.coreChannel = self.amqpconn.channel()
         self.coreChannel.access_request('/data/core', active=True, write=True)
-        self.coreChannel.exchange_declare('events', 'topic', auto_delete=True)
+        self.coreChannel.exchange_declare('myevents', 'topic', auto_delete=True)
+        qname, _, _ = self.coreChannel.queue_declare()
+        self.coreChannel.queue_bind(qname, 'myevents', routing_key="%s.*"%self.domain)
+        self.coreChannel.basic_consume(qname, consumer_tag="coreListener", callback=self._coreCallback)
 
         self.eventChannel = self.amqpconn.channel()
         self.eventChannel.access_request('/data/event', active=True, write=True)
-        self.eventChannel.exchange_declare('events', 'topic', auto_delete=True)
+        self.eventChannel.exchange_declare('myevents', 'topic', auto_delete=True)
+        qname, _, _ = self.eventChannel.queue_declare()
+        self.eventChannel.queue_bind(qname, 'myevents', routing_key="%s.*"%self.domain)
+        self.eventChannel.basic_consume(qname, consumer_tag="eventListener", callback=self._eventCallback)
+
+        mt = threading.Thread(target=self._startCoreListener)
+        mt.setDaemon(True)
+        mt.start()
+    
+        mt = threading.Thread(target=self._startEventListener)
+        mt.setDaemon(True)
+        mt.start()
+    
+    def _startCoreListener(self):
+        try:
+            while self.coreChannel.callbacks:
+                self.coreChannel.wait()
+        except Exception, ex:
+            self.logger.error("Exception raised in event listener thread: %s " % str(ex))
+
+    def _startEventListener(self):
+        try:
+            while self.coreChannel.callbacks:
+                self.coreChannel.wait()
+        except Exception, ex:
+            self.logger.error("Exception raised in event listener thread: %s " % str(ex))
+
+    def _eventCallback(self, msg):
+        for key, val in msg.properties.items():
+            print '%s: %s' % (key, str(val))
+        for key, val in msg.delivery_info.items():
+            print '> %s: %s' % (key, str(val))
+    
+        print ''
+        print msg.body
+        print '-------'
+        msg.channel.basic_ack(msg.delivery_tag)
+
+    def _coreCallback(self, msg):
+        for key, val in msg.properties.items():
+            print '%s: %s' % (key, str(val))
+        for key, val in msg.delivery_info.items():
+            print '> %s: %s' % (key, str(val))
+    
+        print ''
+        print msg.body
+        print '-------'
+        msg.channel.basic_ack(msg.delivery_tag)
 
     def stop(self):
+        self.coreChannel.basic_cancel('coreListener')
+        self.eventChannel.basic_cancel('eventListener')
         self.amqpconn.close()
     
 #    msg = amqp.Message(msg_body, content_type='text/plain', application_headers={'foo': 7, 'bar': 'baz'})
