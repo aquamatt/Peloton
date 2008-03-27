b490381a1ebe2e0f0092e0e7b538d76822537b92
mp <mp@4f9a194a-353e-0410-9ebf-c1faf78b6e41>
2008-03-27 15:30:43 +0000

    removing
    git-svn-id: https://svn.rethought-solutions.com/svn/peloton/trunk@99 4f9a194a-353e-0410-9ebf-c1faf78b6e41

--- src/peloton/persist/store.py
+++
@@ -1,9 +0,0 @@
-# $Id$
-#
-# Copyright (c) 2007-2008 ReThought Limited and Peloton Contributors
-# All Rights Reserved
-# See LICENSE for details
-
-
-""" Interface for Peloton persistent, non-volatile storage. Likely
-adapters include ZODB/ZEO and possibly some SQL store """
--- src/peloton/persist/zeostore.py
+++
@@ -1,8 +0,0 @@
-# $Id$
-#
-# Copyright (c) 2007-2008 ReThought Limited and Peloton Contributors
-# All Rights Reserved
-# See LICENSE for details
-
-
-""" Peloton adapter for persistent storage in ZODB via ZEO """
